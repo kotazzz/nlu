912e1b598e58c6748749515b17b93f20899e37a2
NewLife1324 <61694357+NewLife1324@users.noreply.github.com>
2020-12-15 20:51:26 +0300

    New RandomModule Completed

--- NewLifeUtils.py
+++ NewLifeUtils.py
@@ -894,7 +894,7 @@ class UtilsModule(object):
             try:
                 res += e.code_context[0]
             except Exception as e:
-                res += ' <Unknown> '
+                res += " <Unknown> "
         return res.rstrip("\n").rsplit("\n", 1)[-1]
 
     def __partition(self, nums, low, high):
@@ -1251,6 +1251,44 @@ class DatabaseManageModule(object):
 class RandomModule(object):
     def __init__(self):
         pass
+
+    def format_number(
+        self,
+        number=random.randrange(1111111111111111, 9999999999999999),
+        numform="4444",
+    ):
+        # A total conversion
+        number = int(number)
+        numform = str(numform)
+        # Total length
+        total_length = 0
+        for num in numform:
+            total_length += int(num)
+
+        # Dividing or lengthening a number
+        if number == 0:
+            number = 1
+
+        if len(str(number)) < total_length:
+            while len(str(number)) < total_length:
+                number = number * 10
+        elif len(str(number)) > total_length:
+            number = int(str(number)[0:total_length])
+
+        # Add spaces
+        result = str(number)
+        shift = 0
+        for pos in numform:
+            result = result[: int(pos) + shift] + " " + result[int(pos) + shift :]
+            shift += int(pos) + 1
+        return result
+
+    def set_seed_formated(self, seed=-1):
+        if seed == -1:
+            seed = self.format_number()
+        processed_seed = int("".join(seed.split()))
+        random.seed = processed_seed
+        return processed_seed
 
 
 def testNlu():
