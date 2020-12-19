1b5d95c830a308a53878eb964fad071a2fa7f06c
NewLife1324 <61694357+NewLife1324@users.noreply.github.com>
2020-12-19 20:14:49 +0300

    Update Unnamed project.py

--- Unnamed project.py
+++ Unnamed project.py
@@ -1,1 +1,113 @@
-IyAtKi0gY29kaW5nOiB1dGYtOCAtKi0NCiMgbmV3IHByb2plY3QNCmZyb20gTmV3TGlmZVV0aWxzIGltcG9ydCAqDQppbXBvcnQgcHltb3JwaHkyDQppbXBvcnQgY3N2DQpkZWYgZ2V0X2dlbmRlcihuYW1lKToNCiAgICBnZW5kZXIgPSBweW1vcnBoeTIuTW9ycGhBbmFseXplcigpLnBhcnNlKG5hbWUpWzBdLnRhZy5nZW5kZXINCiAgICBpZiBnZW5kZXIgPT0gJ21hc2MnOg0KICAgICAgICByZXR1cm4gJ20nDQogICAgaWYgZ2VuZGVyID09ICdmZW1uJzoNCiAgICAgICAgcmV0dXJuICdmJw0KICAgICAgICANCmRlZiBnZW5lcmF0ZV9zY2hvb2woKToNCiAgICB0Ym0gPSBUYWJsZUJ1aWxkTW9kdWxlKCkNCiAgICBkYXRhID0gW10NCiAgICB3aXRoIG9wZW4oIlVubmFtZWQgcHJvamVjdCAtIEhTRC5jc3YiLCBuZXdsaW5lPSIiLCBlbmNvZGluZz0idXRmOCIpIGFzIGNzdmZpbGU6DQogICAgICAgIHNwYW1yZWFkZXIgPSBjc3YucmVhZGVyKGNzdmZpbGUsIGRlbGltaXRlcj0iOyIsIHF1b3RlY2hhcj0ifCIpDQogICAgICAgIGZvciByb3cgaW4gc3BhbXJlYWRlcjoNCiAgICAgICAgICAgIGZvciBlbCBpbiByb3c6DQogICAgICAgICAgICAgICAgZGF0YS5hcHBlbmQoc3RyKGVsKSkNCiAgICBtb3JwaCA9IHB5bW9ycGh5Mi5Nb3JwaEFuYWx5emVyKCkNCiAgICBjaXR5X3RfciA9IHRibS5nZXRfY29sdW1uKA0KICAgICAgICB0Ym0uZ2V0X3JlY29yZCg5LCBkYXRhLCByYW5kb20ucmFuZHJhbmdlKDEsIHRibS5nZXRfcmVjb3JkX2NvdW50KDksIGRhdGEpKSksIDksIDMNCiAgICApDQogICAgY2l0eV90ID0gbW9ycGgucGFyc2UoY2l0eV90X3JbMF0pWzBdLmluZmxlY3QoeyJsb2N0In0pLndvcmQNCiAgICBjaXR5X3IgPSB0Ym0uZ2V0X2NvbHVtbigNCiAgICAgICAgdGJtLmdldF9yZWNvcmQoOSwgZGF0YSwgcmFuZG9tLnJhbmRyYW5nZSgxLCB0Ym0uZ2V0X3JlY29yZF9jb3VudCg5LCBkYXRhKSkpLCA5LCAyDQogICAgKQ0KICAgIGNpdHkgPSBjaXR5X3JbMF0NCiAgICBzY2hvb2xfaWQgPSByYW5kb20ucmFuZHJhbmdlKDEsIDE1MCkNCiAgICBzY2hvb2xfbmFtZSA9IGYi0KjQutC+0LvQsCDQsiB7Y2l0eV90fSB7Y2l0eX0gI3tzY2hvb2xfaWR9Ig0KICAgIHJldHVybiBbc2Nob29sX25hbWUsIHNjaG9vbF9pZF0NCg0KICAgICAgICANCmNsYXNzIERhdGFTZWN0b3Iob2JqZWN0KToNCiAgICBkYXRhX3N0b3JhZ2UgPSB7fQ0KICAgIGRlZiBjcmVhdGVfc2VjdGlvbihzZWxmLCBuYW1lLCAqYXJncyk6DQogICAgICAgICAgICB0cnk6DQogICAgICAgICAgICAgICAgc2VsZi5kYXRhX3N0b3JhZ2VbbmFtZV0NCiAgICAgICAgICAgIGV4Y2VwdDoNCiAgICAgICAgICAgICAgICBzZWxmLmRhdGFfc3RvcmFnZVtuYW1lXSA9IFtdDQogICAgICAgICAgICBmb3IgaXRlbSBpbiBhcmdzOg0KICAgICAgICAgICAgICAgIHNlbGYuZGF0YV9zdG9yYWdlW25hbWVdLmFwcGVuZChpdGVtKQ0KICAgIGRlZiBnZXRfc3RvcmFnZShzZWxmKToNCiAgICAgICAgcmV0dXJuKHNlbGYuZGF0YV9zdG9yYWdlKQ0KICAgIGRlZiBnZXRfc2VjdGlvbihzZWxmLCBzZWN0aW9uX25hbWUpOg0KICAgICAgICByZXR1cm4oc2VsZi5kYXRhX3N0b3JhZ2Vbc2VjdGlvbl9uYW1lXSkNCg0KY2xhc3MgRGVmU2VjdG9yOg0KICAgIGNsYXNzIFNjaG9vbChvYmplY3QpOg0KICAgICAgICBkZWYgX19pbml0X18oc2VsZik6DQogICAgICAgICAgICBzY2hvb2xkYXRhID0gZ2VuZXJhdGVfc2Nob29sKCkNCiAgICAgICAgICAgIHNlbGYuc2Nob29sX25hbWUgPSBzY2hvb2xkYXRhWzBdDQogICAgICAgICAgICBzZWxmLnNjaG9vbF9udW1iZXIgPSBzY2hvb2xkYXRhWzFdDQogICAgICAgICAgICBzZWxmLnNjaG9vbF9pZCA9ICcxMjMnDQogICAgY2xhc3MgQ2FiaW5ldChvYmplY3QpOg0KICAgICAgICByb29tX3R5cGUgPSAnZ2VuZXJhbCcNCiAgICBjbGFzcyBDbGFzc0NhYmluZXQoQ2FiaW5ldCk6DQogICAgICAgIHNwZWNpZmljYXRpb24gPSAnY2hlbWlzdHJ5Jw0KICAgICAgICANCiAgICBjbGFzcyBQZXJzb24ob2JqZWN0KToNCiAgICAgICAgbmFtZSA9ICfQkNGA0LrQsNC00LjQuScNCiAgICAgICAgc3VybmFtZSA9ICfQmtC+0YLQsNC30L7QsicNCiAgICAgICAgcGF0cm9ueW1pYyA9ICfQnNC40YXQsNC40LvQvtCy0LjRhycNCiAgICAgICAgDQogICAgY2xhc3MgU3RhZmYoUGVyc29uKToNCiAgICAgICAgc3RhZmZfdHlwZSA9ICdkaXJlY3RvcicNCiAgICBjbGFzcyBUZWFjaGVyKFBlcnNvbik6DQogICAgICAgIHNjaG9vbF9zdWJqZWN0cyA9IFsn0JzQsNGC0LXQvNCw0YLQuNC60LAnLCAn0KTQuNC30LjQutCwJ10NCiAgICBjbGFzcyBTdHVkZW50KFBlcnNvbik6DQogICAgICAgIGdvYWwgPSB7InR5cGUiOiJzdHVkIn0NCiAgICAgICAgDQogICAgY2xhc3MgU3ViamVjdDoNCiAgICAgICAgcGFzcw0KICAgICAgICANCmNsYXNzIENvbW1hbmRzU2VjdG9yOg0KICAgIGNsYXNzIEluZm8oQ3VzdG9tU2hlbGxNb2R1bGUuQ29tbWFuZCk6DQogICAgICAgIGNvbW1hbmQgPSAi0LjQvdGE0L4iDQogICAgICAgIGFsaWFzZXMgPSBbY29tbWFuZCwgItC40L3RhNC+0YDQvNCw0YbQuNGPIl0NCiAgICAgICAgZGVzY3JpcHRpb24gPSAi0JLRgdGPINC40L3RhNC+0YDQvNCw0YbQuNGPINC+INC60L7QvNC80LDQvdC00LDRhSINCiAgICAgICAgcmVxdWlyZWQgPSBbXQ0KICAgICAgICBvcHRpb25hbCA9IFtdDQogICAgICAgIHNraXBjaGVjayA9IEZhbHNlDQogICAgICAgIGRlZiBydW4oY29uc29sZSk6DQogICAgICAgICAgICBpbmZvID0gJ1xuLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tXG4nDQogICAgICAgICAgICBpbmZvICs9ICfQrdGC0L4g0LzQvtC5INC90L7QstGL0Lkg0L/RgNC+0LXQutGCINC+0YHQvdC+0LLQsNC90L3Ri9C5INC90LAgTkxVNVxuJw0KICAgICAgICAgICAgaW5mbyArPSAn0JHQvtC70YzRiNC1INC90LXRgiDQtNC+0YHRgtGD0L/QvdC+0Lkg0LjQvdGE0L7RgNC80LDRhtC40LhcbicNCiAgICAgICAgICAgIGluZm8gKz0gJy0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLVxuJw0KICAgICAgICAgICAgY29uc29sZS5Mb2dnZXIubG9nKGluZm8pDQpkZWYgb3RoZXIoKToNCiAgICBjbGFzcyBUYXNrOg0KICAgICAgICBkZWYgZXhlY3V0ZShjb25zb2xlKToNCiAgICAgICAgICAgIGxvZ2dlciA9IExvZ2dlck1vZHVsZSgpDQogICAgICAgICAgICBQZXJzb25JbmZvID0gRGF0YVNlY3RvcigpDQogICAgICAgICAgICBkYXRhZmlsZSA9b3BlbignVW5uYW1lZCBwcm9qZWN0IC0gZGF0YS5qc29uJywgJ3InLCBlbmNvZGluZz0ndXRmOCcpDQogICAgICAgICAgICBkYXRhID0ganNvbi5sb2FkcyhkYXRhZmlsZS5yZWFkKCkpDQogICAgICAgICAgICBQZXJzb25JbmZvLmNyZWF0ZV9zZWN0aW9uKCduYW1lcycsZGF0YVsnbmFtZXMnXSkNCiAgICAgICAgICAgIFBlcnNvbkluZm8uY3JlYXRlX3NlY3Rpb24oJ3N1cm5hbWVzJyxkYXRhWydzdXJuYW1lcyddKQ0KICAgICAgICAgICAgUGVyc29uSW5mby5jcmVhdGVfc2VjdGlvbigncGF0cm9ueW1pY3MnLGRhdGFbJ3BhdHJvbnltaWNzJ10pDQogICAgICAgICAgICANCiAgICAjSW5pdCBDdXN0b21TaGVsbE1vZHVsZQ0KICAgIGNtZCA9IEN1c3RvbVNoZWxsTW9kdWxlKCkNCiAgICANCiAgICAjUmVnaXN0ZXIgY29tbWFuZHMNCiAgICBjbWQucmVnaXN0ZXJDb21tYW5kKENvbW1hbmRzU2VjdG9yLkluZm8pDQogICAgDQogICAgI1JlZ2lzdGVyIHRhc2tzDQogICAgY21kLnJlZ2lzdGVySW5pdFRhc2soVGFzaykNCiAgICANCiAgICAjc3RhcnQgbWFpbiBjeWNsZQ0KICAgIGNtZC5tYWluKCkNCmlmIF9fbmFtZV9fID09ICJfX21haW5fXyI6DQogICAgbXlzY2hvb2wgPSBTY2hvb2woKQ
+# -*- coding: utf-8 -*-
+# new project
+from NewLifeUtils import *
+import pymorphy2
+import csv
+def get_gender(name):
+    gender = pymorphy2.MorphAnalyzer().parse(name)[0].tag.gender
+    if gender == 'masc':
+        return 'm'
+    if gender == 'femn':
+        return 'f'
+        
+def generate_school():
+    tbm = TableBuildModule()
+    data = []
+    with open("Unnamed project - HSD.csv", newline="", encoding="utf8") as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=";", quotechar="|")
+        for row in spamreader:
+            for el in row:
+                data.append(str(el))
+    morph = pymorphy2.MorphAnalyzer()
+    city_t_r = tbm.get_column(
+        tbm.get_record(9, data, random.randrange(1, tbm.get_record_count(9, data))), 9, 3
+    )
+    city_t = morph.parse(city_t_r[0])[0].inflect({"loct"}).word
+    city_r = tbm.get_column(
+        tbm.get_record(9, data, random.randrange(1, tbm.get_record_count(9, data))), 9, 2
+    )
+    city = city_r[0]
+    school_id = random.randrange(1, 150)
+    school_name = f"Школа в {city_t} {city} #{school_id}"
+    return [school_name, school_id]
+
+        
+class DataSector(object):
+    data_storage = {}
+    def create_section(self, name, *args):
+            try:
+                self.data_storage[name]
+            except:
+                self.data_storage[name] = []
+            for item in args:
+                self.data_storage[name].append(item)
+    def get_storage(self):
+        return(self.data_storage)
+    def get_section(self, section_name):
+        return(self.data_storage[section_name])
+
+class DefSector:
+    class School(object):
+        def __init__(self):
+            schooldata = generate_school()
+            self.school_name = schooldata[0]
+            self.school_number = schooldata[1]
+            self.school_id = '123'
+    class Cabinet(object):
+        room_type = 'general'
+    class ClassCabinet(Cabinet):
+        specification = 'chemistry'
+        
+    class Person(object):
+        name = 'Аркадий'
+        surname = 'Котазов'
+        patronymic = 'Михаилович'
+        
+    class Staff(Person):
+        staff_type = 'director'
+    class Teacher(Person):
+        school_subjects = ['Математика', 'Физика']
+    class Student(Person):
+        goal = {"type":"stud"}
+        
+    class Subject:
+        pass
+        
+class CommandsSector:
+    class Info(CustomShellModule.Command):
+        command = "инфо"
+        aliases = [command, "информация"]
+        description = "Вся информация о коммандах"
+        required = []
+        optional = []
+        skipcheck = False
+        def run(console):
+            info = '\n---------------------------------------------\n'
+            info += 'Это мой новый проект основанный на NLU5\n'
+            info += 'Больше нет доступной информации\n'
+            info += '---------------------------------------------\n'
+            console.Logger.log(info)
+def other():
+    class Task:
+        def execute(console):
+            logger = LoggerModule()
+            PersonInfo = DataSector()
+            datafile =open('Unnamed project - data.json', 'r', encoding='utf8')
+            data = json.loads(datafile.read())
+            PersonInfo.create_section('names',data['names'])
+            PersonInfo.create_section('surnames',data['surnames'])
+            PersonInfo.create_section('patronymics',data['patronymics'])
+            
+    #Init CustomShellModule
+    cmd = CustomShellModule()
+    
+    #Register commands
+    cmd.registerCommand(CommandsSector.Info)
+    
+    #Register tasks
+    cmd.registerInitTask(Task)
+    
+    #start main cycle
+    cmd.main()
+if __name__ == "__main__":
+    myschool = DefSector.School()
