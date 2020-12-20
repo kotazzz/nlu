570c075053da7522ee60a68854412861d44672cd
NewLife1324 <61694357+NewLife1324@users.noreply.github.com>
2020-12-20 16:34:12 +0300

    More Modules

--- CompileDist/test.py
+++ CompileDist/test.py
@@ -1,12 +1,22 @@
 import NewLifeUtils.Modules.ColorModule as cm
 import NewLifeUtils.Modules.StringUtilModule as sm
 import NewLifeUtils.Modules.LoggerModule as lm
+import NewLifeUtils.Modules.ExceptModule as em
+import NewLifeUtils.Modules.RandomModule as rm
+import NewLifeUtils.Modules.TableBuildModule as tbm
+import NewLifeUtils.Modules.CustomShellModule as csm
 
 print('|'+sm.screate(f'{cm.FGC.RED}HELLO{cm.ACC.RESET}')+'|')
 lm.log('hi')
-lm.rea('hi')
+s = lm.rea('hi')
 lm.log('hi')
 lm.log('hi')
 lm.log('hi')
 lm.log('hi')
 lm.log('hi')
+em.except_print(Exception(s), 'wrn')
+data = ['1','2','3','4','5','6']
+t = tbm.createTable(2,[],data)
+print(t)
+print(rm.format_number())
+csm.Shell().main()
