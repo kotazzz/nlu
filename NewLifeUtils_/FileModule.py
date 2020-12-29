00abb74958a99444cce888c8cbb3c93ba97f2305
NewLife1324 <61694357+NewLife1324@users.noreply.github.com>
2020-12-29 21:04:05 +0300

    Logger dev

--- NewLifeUtils_/FileModule.py
+++ NewLifeUtils_/FileModule.py
@@ -4,32 +4,34 @@ wd_name = "NLU Config"
 cwd = os.path.join(os.getcwd(), wd_name)
 
 configs = {}
-
-if not os.path.exists(cwd):
-    os.makedirs(cwd)
+def create_dirs(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 def create_config(configname, file, path = ""):
+    fullpath = os.path.join(os.path.join(cwd, path), '')
+    create_dirs(fullpath)
     try:
-        f = open(os.path.join(cwd, path)+f'{file}', 'a')
+        f = open(fullpath+f'\\{file}', 'a')
     except:
-        f = open(os.path.join(cwd, path)+f'{file}', 'w')
-    configs[configname] = os.path.join(cwd, path)+f'{file}'
+        f = open(fullpath+f'\\{file}', 'w')
+    configs[configname] = os.path.join(cwd, path)+f'\\{file}'
     f.close()
     
-def get_configs():
+def config_get_configs():
     return configs
 
-def rewrite(configname, data):
+def config_rewrite(configname, data):
     f = open(configs[configname], 'w')
     f.write(data)
     f.close()
     
-def apwrite(configname, data):
+def config_apwrite(configname, data):
     f = open(configs[configname], 'a')
     f.write(data)
     f.close()
 
-def readall(configname):
+def config_readall(configname):
     f = open(configs[configname], 'r')
     r = f.read()
     f.close()
@@ -38,7 +40,7 @@ def readall(configname):
 if __name__ == "__main__":
     create_config("cfg","mycfg.txt")
     create_config("cfg2","mycfg2.txt")
-    print(get_configs())
-    print(readall("cfg2"))
+    print(config_get_configs())
+    print(config_readall("cfg2"))
     
     
