182db363a03b4a518b11938119479923f744f35f
NewLife1324 <61694357+NewLife1324@users.noreply.github.com>
2021-01-25 22:29:32 +0300

    Completed first completer

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 from pynput.keyboard import Key, Listener
 from NewLifeUtils.ColorModule import MCC, ACC
 import pynput
@@ -6,22 +5,34 @@ import sys
 from msvcrt import getwch
 
 from NewLifeUtils.ColorModule import FGC
-from NewLifeUtils.StringUtilModule import screate, parse_args
+from NewLifeUtils.StringUtilModule import screate, parse_args, remove_csi
 
 selector = 0
 
-
+current_text = ''
 def complete(readed, completes):
     global selector
-    print(f'{ACC.CLEARSCREEN}')
-    parsed = parse_args(readed)
+    global current_text
+    #print(f'{ACC.CLEARSCREEN}')
+    if current_text == '':
+        current_text = readed
+    work_parsed = parse_args(readed)["split"]
+    if readed != '':
+        if readed[-1] == ' ':
+            work_parsed.append('')
+
+
     keys = completes.keys()
 
-    for arg in parsed["split"]:
+
+    #print(readed)
+    #print(current_text)
+    aval = []
+    for argnum, arg in enumerate(work_parsed, start=1):
         aval = []
         next_sel = False
         for key in keys:
-            print(f'{FGC.GREEN}arg: {arg}, key: {key}, result: {key.startswith(arg)}')
+            #print(f'{FGC.GREEN}arg: {arg}, key: {key}, result: {key.startswith(arg)}')
             sys.stdout.flush()
             if key.startswith(arg):
                 aval.append(key)
@@ -35,16 +46,19 @@ def complete(readed, completes):
                     sys.stdout.flush()
                     keys = completes[key]
                     next_sel = True
-            elif key == arg:
-                keys = completes[key]
-                break
+
         if next_sel:
             selector += 1
             selector %= len(aval)
-        if len(aval) > 0:
-            print(f'{FGC.MAGENTA}com: {aval} ({keys}), CURRENT: {selector} - {aval[selector]}')
+        if len(aval) == 1:
+            work_parsed[argnum - 1] = aval[0]
+        elif len(aval) > 0:
+            work_parsed[argnum - 1] = aval[selector]
+            #print(f'{FGC.MAGENTA}com: {aval}, next: {keys}, CURRENT: {selector} - {aval[selector]}')
 
-    return readed
+    #print(ACC.RESET)
+
+    return ' '.join(work_parsed), aval
 
 
 def smart_input(text='', completes={}, end='\n'):
@@ -54,9 +68,9 @@ def smart_input(text='', completes={}, end='\n'):
     while True:
         key = getwch()
         if ord(key) == 224:
-            print(ord(getwch()))
+            pass
         elif ord(key) == 0:
-            print(ord(getwch()))
+            pass
         else:
             if ord(key) == 8:
                 readed = readed[:-1]
@@ -65,8 +79,13 @@ def smart_input(text='', completes={}, end='\n'):
             elif ord(key) == 13:
                 break
             elif ord(key) == 9:
-                readed = complete(readed, completes)
-                print(MCC.load_cursor + MCC.erase_nxt_line + readed, end='')
+                readed, aval = complete(readed, completes)
+                if len(aval) > 0:
+                    avalr = ', '.join(aval)
+                else:
+                    avalr = 'no suggestion'
+                avaltext = f'{FGC.GRAY} ({avalr}) {ACC.RESET}'
+                print(MCC.load_cursor + MCC.erase_nxt_line + readed + avaltext + MCC.left(len(remove_csi(avaltext))), end='')
             else:
                 readed += key
                 print(MCC.load_cursor + MCC.erase_nxt_line + readed, end='')
@@ -78,128 +97,3 @@ def smart_input(text='', completes={}, end='\n'):
 if __name__ == '__main__':
     inp = smart_input('Введите текст:', {"hello": {"world": {}, "me": {}}, "hi": {"friend": {}}, "hem": {}})
     print(f'Вы ввели: {inp}')
-
-#
-# KEYS = {
-#    "LBUTTON": 1,
-#    "RBUTTON": 2,
-#    "CANCEL": 3,
-#    "MBUTTON": 4,
-#    "BACK": 8,
-#    "TAB": 9,
-#    "CLEAR": 12,
-#    "RETURN": 13,
-#    "SHIFT": 16,
-#    "CONTROL": 17,
-#    "MENU": 18,
-#    "PAUSE": 19,
-#    "CAPITAL": 20,
-#    "KANA": 21,
-#    "HANGEUL": 21,
-#    "HANGUL": 21,
-#    "JUNJA": 23,
-#    "FINAL": 24,
-#    "HANJA": 25,
-#    "KANJI": 25,
-#    "ESCAPE": 27,
-#    "CONVERT": 28,
-#    "NONCONVERT": 29,
-#    "ACCEPT": 30,
-#    "MODECHANGE": 31,
-#    "SPACE": 32,
-#    "PRIOR": 33,
-#    "NEXT": 34,
-#    "END": 35,
-#    "HOME": 36,
-#    "LEFT": 37,
-#    "UP": 38,
-#    "RIGHT": 39,
-#    "DOWN": 40,
-#    "SELECT": 41,
-#    "PRINT": 42,
-#    "EXECUTE": 43,
-#    "SNAPSHOT": 44,
-#    "INSERT": 45,
-#    "DELETE": 46,
-#    "HELP": 47,
-#    "LWIN": 91,
-#    "RWIN": 92,
-#    "APPS": 93,
-#    "NUMPAD0": 96,
-#    "NUMPAD1": 97,
-#    "NUMPAD2": 98,
-#    "NUMPAD3": 99,
-#    "NUMPAD4": 100,
-#    "NUMPAD5": 101,
-#    "NUMPAD6": 102,
-#    "NUMPAD7": 103,
-#    "NUMPAD8": 104,
-#    "NUMPAD9": 105,
-#    "MULTIPLY": 106,
-#    "ADD": 107,
-#    "SEPARATOR": 108,
-#    "SUBTRACT": 109,
-#    "DECIMAL": 110,
-#    "DIVIDE": 111,
-#    "F1": 112,
-#    "F2": 113,
-#    "F3": 114,
-#    "F4": 115,
-#    "F5": 116,
-#    "F6": 117,
-#    "F7": 118,
-#    "F8": 119,
-#    "F9": 120,
-#    "F10": 121,
-#    "F11": 122,
-#    "F12": 123,
-#    "F13": 124,
-#    "F14": 125,
-#    "F15": 126,
-#    "F16": 127,
-#    "F17": 128,
-#    "F18": 129,
-#    "F19": 130,
-#    "F20": 131,
-#    "F21": 132,
-#    "F22": 133,
-#    "F23": 134,
-#    "F24": 135,
-#    "NUMLOCK": 144,
-#    "SCROLL": 145,
-#    "LSHIFT": 160,
-#    "RSHIFT": 161,
-#    "LCONTROL": 162,
-#    "RCONTROL": 163,
-#    "LMENU": 164,
-#    "RMENU": 165,
-#    "PROCESSKEY": 229,
-#    "ATTN": 246,
-#    "CRSEL": 247,
-#    "EXSEL": 248,
-#    "EREOF": 249,
-#    "PLAY": 250,
-#    "ZOOM": 251,
-#    "NONAME": 252,
-#    "PA1": 253,
-#    "OEM_CLEAR": 254,
-# }
-# text = ''
-#
-# while True:
-#    cp = win32api.GetCursorPos()
-#    result = ''
-#    sys.stdout.write(ACC.CLEARSCREEN)
-#
-#    for key in KEYS:
-#        state = win32api.GetKeyState(KEYS[key])
-#        if state != 0:
-#            text = '    pressed'
-#        else:
-#            text = 'not pressed'
-#        result += f"{screate(key, 10)}:{text}-{screate(str(state), 4)}; "
-#    sys.stdout.write(result+f'\ncp: {cp}')
-#    sleep(0.4)
-#
-#
-#
