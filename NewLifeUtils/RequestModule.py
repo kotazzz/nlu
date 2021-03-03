0ddeadd7add26bdafb9dcc5f4eb7301f9bebd7fb
NewLife1324 <61694357+NewLife1324@users.noreply.github.com>
2021-03-03 19:20:58 +0300

    Added new vk_simple_get() (RM)

--- NewLifeUtils/RequestModule.py
+++ NewLifeUtils/RequestModule.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 history = []
 
@@ -13,3 +14,12 @@ def req_get(url, params={}):
     resp = requests.get(url_out)
     global id_counter
     history.append({"url": url_out, resp: "resp"})
+
+
+
+def vk_simple_get(method, args, gid, token, ver):
+    url = f'https://api.vk.com/method/{method}?'
+    for arg in args:
+        url += f'{arg}={args[arg]}&'
+    response = json.loads(requests.get(url + f'group_id={gid}&access_token={token}&v={ver}').text)
+    return response
