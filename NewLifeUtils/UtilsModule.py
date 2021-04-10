ea59ea688375d76b004de5e2eb2b28b805eea316
NewLife1324 <61694357+NewLife1324@users.noreply.github.com>
2021-04-10 17:34:29 +0300

    Remove unused functions

--- NewLifeUtils/UtilsModule.py
+++ NewLifeUtils/UtilsModule.py
@@ -2,41 +2,8 @@ import inspect
 import random
 import re
 
-from NewLifeUtils.LoggerModule import *
+
 from NewLifeUtils.StringUtilModule import parse_args
-
-
-def select_rand_list(source, use_zero=False):
-    try:
-        source[0]
-    except IndexError:
-        err(f"{source} is empty", "RS ERROR")
-    except TypeError:
-        err(f"{source} object is not subscriptable", "RS ERROR")
-    else:
-        if use_zero:
-            log("Can use zero", "RS LOG")
-            selector = random.randrange(1, len(source))
-        else:
-            log("Cant use zero", "RS LOG")
-            selector = random.randrange(0, len(source))
-        log(
-            f"Select item #{selector} from {type(source).__name__}; result {source[selector]}",
-            "RS LOG",
-        )
-        return source[selector]
-    return None
-
-
-def getfromfname(a):
-    res = ""
-    for e in inspect.stack():
-        try:
-            res += e.code_context[0]
-        except Exception:
-            res += " <Unknown>"
-    return res.rstrip("\n").rsplit("\n", 1)[-1]
-
 
 def __partition(nums, low, high):
     # Выбираем средний элемент в качестве опорного
@@ -74,39 +41,6 @@ def quick_sort(nums):
     _quick_sort(nums, 0, len(nums) - 1)
 
 
-def bprint(text, maxlinelength=9, font="default"):
-    print(get_files_from_dir("fonts"))
-
-
-def format_number(
-    number=random.randrange(1111111111111111, 9999999999999999),
-    numform="4444",
-):
-    # A total conversion
-    number = int(number)
-    numform = str(numform)
-    # Total length
-    total_length = 0
-    for num in numform:
-        total_length += int(num)
-
-    # Dividing or lengthening a number
-    if number == 0:
-        number = 1
-
-    if len(str(number)) < total_length:
-        while len(str(number)) < total_length:
-            number = number * 10
-    elif len(str(number)) > total_length:
-        number = int(str(number)[0:total_length])
-
-    # Add spaces
-    result = str(number)
-    shift = 0
-    for pos in numform:
-        result = result[: int(pos) + shift] + " " + result[int(pos) + shift :]
-        shift += int(pos) + 1
-    return result
 
 
 def safe_format(text, keys=None, func=None, smart=None):
@@ -131,14 +65,6 @@ def safe_format(text, keys=None, func=None, smart=None):
                 return "{" + key + "}"
 
     return text.format_map(SafeDict(**keys))
-
-
-def set_seed_formated(seed=-1):
-    if seed == -1:
-        seed = format_number()
-    processed_seed = int("".join(seed.split()))
-    random.seed = processed_seed
-    return processed_seed
 
 
 def hex_to_rgb(hx, hsl=False):
